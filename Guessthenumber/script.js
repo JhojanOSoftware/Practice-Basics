5bde98ac63a444435d0ecd62e847f0dfbfd2ee34
Fackphenom <jhojanortiz55bogota@gmail.com>
2025-07-11 22:57:30 -0500

    update guess the number

--- Guessthenumber/script.js
+++ Guessthenumber/script.js
@@ -1,16 +1,22 @@
 let random = Math.floor(Math.random() * 5) + 1;
 function verificar(){
-    const guess  = parseInt(document.getElementById("guess").value);
+    const guess  = (document.getElementById("guess").value);
     let result = '';
-    if (guess< random){
-        result= "Too low, Try Again";
-    }else if (guess > random){
-        result = "Too high, Try Again";
+    if (!isNaN(guess) && guess >=1 && guess <=10) {
+        if (guess< random){
+            result= "Too low, Try Again";
+        }else if (guess > random){
+            result = "Too high, Try Again";
+        }else { 
+            result = "You got it buddy!";
+            document.getElementById("reiniciar").style.display = "inline";
+        }
     }else { 
-        result = "You got it buddy!";
-        document.getElementById("reiniciar").style.display = "inline";
+        result = "Please enter a valid number";
     }
-    document.getElementById("result").innerText = result;
+            document.getElementById("result").innerText = result;
+
+    
 }
 function reiniciar(){
     random = Math.floor(Math.random()*10 )+1;
