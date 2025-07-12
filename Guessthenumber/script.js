64a39df8dc92f291c8b07f10815b0173c3239788
Fackphenom <jhojanortiz55bogota@gmail.com>
2025-07-11 22:33:13 -0500

    update Guess the number

--- Guessthenumber/script.js
+++ Guessthenumber/script.js
@@ -8,13 +8,14 @@ function verificar(){
         result = "Too high, Try Again";
     }else { 
         result = "You got it buddy!";
+        document.getElementById("reiniciar").style.display = "inline";
     }
-
-
-
-
-
-
-
     document.getElementById("result").innerText = result;
+}
+function reiniciar(){
+    random = Math.floor(Math.random()*10 )+1;
+    document.getElementById("guess").value ='';
+    document.getElementById("result").innerText='';
+    document.getElementById("guess").disabled = false;
+    document.getElementById("reiniciar").style.display = "none";
 }
