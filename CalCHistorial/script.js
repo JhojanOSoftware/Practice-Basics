2bc0eaec223cd85bf2b655dd87633a435c9920b8
Fackphenom <jhojanortiz55bogota@gmail.com>
2025-07-13 23:15:30 -0500

    Calculator of max and min

---
+++ CalCHistorial/script.js
@@ -0,0 +1,44 @@
+let numeroslist = [];
+function agregar() {
+    if (numeroslist.length>= 5){
+        document.getElementById("mensaje").innerText = "Solo puedes agregar 5 numeros";
+        document.getElementById("mensaje").style.color = "red";
+        document.getElementById("agregar").style.display = "none";
+        return;
+    }
+    const numero = parseFloat(document.getElementById("Numero").value);
+    if (!isNaN(numero)) {
+        numeroslist.push(numero);
+        const item =document.createElement("li");
+        item.innerText= numero;
+        document.getElementById("historial").appendChild(item);
+        if (numeroslist.length>= 5){
+            document.getElementById("agregar").style.display = "none";
+        }
+    }
+}
+function calcular() {
+    if (numeroslist.length === 0){     
+        return 0;
+    }
+    let suma = numeroslist.reduce((acc,numero) => acc+numero,0);
+    let maxenlst = Math.max(...numeroslist);
+    let mienlist =Math.min(...numeroslist);
+    let promedio = suma / numeroslist.length;
+    document.getElementById("resultado").innerHTML =  `Promedio: ${promedio.toFixed(2)} | Máximo: ${maxenlst} | Mínimo: ${mienlist}`;
+}
+function limpiar() {
+    numeroslist = [];
+    document.getElementById("historial").innerHTML = '';
+    document.getElementById("Numero").value = '';
+    document.getElementById("resultado").innerText = '';
+    document.getElementById("mensaje").innerText= '';
+    document.getElementById("agregar").disabled = false;
+
+
+}
+     
+
+
+
+
